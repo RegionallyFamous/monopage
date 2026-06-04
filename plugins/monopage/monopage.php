b0d7996a7f7a1691f8960d8289b5a1dbd2abb5f8
Nick Hamze <me@nickhamze.com>
2026-06-04 17:18:36 -0500

    Update author metadata

--- plugins/monopage/monopage.php
+++ plugins/monopage/monopage.php
@@ -6,7 +6,7 @@
  * Version:           0.2.45
  * Requires at least: 6.5
  * Requires PHP:      7.4
- * Author:            WeirdPress
+ * Author:            Regionally Famous
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       monopage
